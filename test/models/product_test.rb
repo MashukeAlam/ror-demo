d17aa723bd53a8e8831b5cc77012b512113ce37e
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-05 10:19:21 +0600

    written image_url testing code added

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  def newproduct image_url
+    product = Product.new(title: "Jim",
+      description: "yyy",
+      image_url: image_url,
+      price: 2)
+    return product
+  end
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -18,6 +26,18 @@ class ProductTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
       product.errors[:price]
+
+    product.price = 1.5
+    assert product.valid?
+  end
+
+  test "image url should end in proper extensions" do
+    assert newproduct("jim.jpg").valid?
+    assert newproduct("jim.png").valid?
+    assert newproduct("jim.gif").valid?
+    assert newproduct("jim.txt").invalid?
+    assert newproduct("jim.jpeg").invalid?
+    assert newproduct("jim.mp3").valid?
   end
 
 end
