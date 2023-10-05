13ea4af5fadcdd408c269d20ff4e8c3eff41b724
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-05 12:41:14 +0600

    store get index test written

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StoreControllerTest < ActionDispatch::IntegrationTest
+  test "shoud get index" do
+    get store_index_url
+    assert_response :success
+  end
+
 
 
 end
