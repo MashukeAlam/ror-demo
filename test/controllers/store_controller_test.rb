16aa3bbb44581de99e4b356fb316c0410be6c3e4
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-05 12:47:48 +0600

    more tests conducted in 'store_controller_test.rb' file to see if some html elements are correct in number and format.

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,6 +6,14 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-
+  test "check number of html elements" do
+    get store_index_url
+    assert_response :success
+    # grab all links inside nav with class side_nav and count if there are at least 4 of them
+    assert_select 'nav.side_nav a', minimum: 4
+    # check if there are exactly 3 products
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
+  end
 
 end
