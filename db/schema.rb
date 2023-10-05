be4684ec8938962391fde3dab3b8d62594272c66
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-05 13:16:23 +0600

    added a scaffold for carts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_04_093830) do
+ActiveRecord::Schema.define(version: 2023_10_05_071508) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
