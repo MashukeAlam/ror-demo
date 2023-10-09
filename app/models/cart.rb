8779e99a129682e26fa82966b1826d14ef1a811d
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-09 10:12:08 +0600

    added method to increment quantity if already present in line_items database

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    curr = line_items.find_by(product_id: product.id)
+
+    if curr
+      curr.quantity += 1
+    else
+      curr = line_items.build(product_id: product.id)
+    end
+    curr
+  end
 end
