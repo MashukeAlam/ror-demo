e6ab0af6001f53faa4613d01f98fc9f56bad8087
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-12 17:35:29 +0600

    added streamed html updated data on to the store channel.

--- app/javascript/channels/products_channel.js
+++ app/javascript/channels/products_channel.js
@@ -3,6 +3,7 @@ import consumer from "./consumer"
 consumer.subscriptions.create("ProductsChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("yo!");
   },
 
   disconnected() {
@@ -11,5 +12,9 @@ consumer.subscriptions.create("ProductsChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-  }
+      const storeElement = document.querySelector("main.store")
+      if (storeElement) {
+         storeElement.innerHTML = data.html
+      }
+    }
 });
