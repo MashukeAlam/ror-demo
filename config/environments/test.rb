02a7f35f8971ef8adf468e6da3193662ee2a5621
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-18 12:20:00 +0600

    email config added

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,6 +43,15 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "dave",
+    password: "secret",
+    enable_starttls_auto: true
+  }
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
