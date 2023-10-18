cc672388804664af449d0b90463e52250f17a2ec
Mashuke Alam Jim <jimmashuke@gmail.com>
2023-10-18 10:39:18 +0600

    DOMContentLoaded replaced turbolinks. for some reason turbolinks not working

--- app/javascript/PayTypeSelector/NoPayType.jsx
+++ app/javascript/PayTypeSelector/NoPayType.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 class NoPayType extends React.Component {
     render() {
-        return (<div></div>);
+        return (<div>yy</div>);
     }
 }
 export default NoPayType
--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PayTypeSelector from 'PayTypeSelector';
+import PayTypeSelector from '../PayTypeSelector';
+import {createRoot} from "react-dom/client";
 
-document.addEventListener('turbolinks.load', () => {
-    var element = document.getElementById("pay-type-component");
-    ReactDOM.render(<PayTypeSelector />, element);
+
+document.addEventListener('DOMContentLoaded', () => {
+    let element = document.getElementById("pay-type-component");
+    const root = createRoot(element);
+    root.render(<PayTypeSelector />)
 });
